6c2599b9c1b6b5607e0fb546714541faea522e50
John Doe <jdoe@email.com>
2017-05-17 14:47:36 +0200

    1.00 get last news by app id

---
+++ SteamExplorer/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace SteamExplorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lbltitle = new MetroFramework.Controls.MetroLabel();
+            this.LblContent = new MetroFramework.Controls.MetroLabel();
+            this.WbContent = new System.Windows.Forms.WebBrowser();
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.metroPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Lbltitle
+            // 
+            this.Lbltitle.AutoSize = true;
+            this.Lbltitle.Location = new System.Drawing.Point(24, 64);
+            this.Lbltitle.Name = "Lbltitle";
+            this.Lbltitle.Size = new System.Drawing.Size(33, 19);
+            this.Lbltitle.TabIndex = 0;
+            this.Lbltitle.Text = "Title";
+            // 
+            // LblContent
+            // 
+            this.LblContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LblContent.AutoSize = true;
+            this.LblContent.Location = new System.Drawing.Point(24, 113);
+            this.LblContent.Name = "LblContent";
+            this.LblContent.Size = new System.Drawing.Size(55, 19);
+            this.LblContent.TabIndex = 30;
+            this.LblContent.Text = "Content";
+            // 
+            // WbContent
+            // 
+            this.WbContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.WbContent.Location = new System.Drawing.Point(0, 0);
+            this.WbContent.MinimumSize = new System.Drawing.Size(20, 20);
+            this.WbContent.Name = "WbContent";
+            this.WbContent.Size = new System.Drawing.Size(647, 308);
+            this.WbContent.TabIndex = 31;
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroPanel1.Controls.Add(this.WbContent);
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(23, 96);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(647, 308);
+            this.metroPanel1.TabIndex = 32;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 415);
+            this.Controls.Add(this.LblContent);
+            this.Controls.Add(this.Lbltitle);
+            this.Controls.Add(this.metroPanel1);
+            this.Name = "Form1";
+            this.Text = "News";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.metroPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroLabel Lbltitle;
+        private MetroFramework.Controls.MetroLabel LblContent;
+        private System.Windows.Forms.WebBrowser WbContent;
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+    }
+}
+
---
+++ SteamExplorer/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SteamKit2;
+
+namespace SteamExplorer
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Lbltitle.Text = "Loading...";
+            LblContent.Text = "";
+            System.Threading.Timer timer = null;
+            timer = new System.Threading.Timer((obj) =>
+            {
+                init();
+                timer.Dispose();
+            },
+                null, 1000,500);
+            
+
+        }
+        public void init()
+        {
+            
+            dynamic steamNews = WebAPI.GetInterface("ISteamNews");
+            KeyValue kvNews = steamNews.GetNewsForApp(appid: 730); // get news for csgo
+                                                                   // the return of every WebAPI call is a KeyValue class that contains the result data
+
+            // for this example we'll iterate the results and display the title
+            foreach (KeyValue news in kvNews["newsitems"]["newsitem"].Children)
+            {
+
+                Lbltitle.Invoke (new Action(() => Lbltitle.Text = "News: {0}\n" + news["title"].AsString()));
+                WbContent.Invoke(new Action(() => WbContent.DocumentText = "0"));
+                WbContent.Invoke(new Action(() => WbContent.Document.OpenNew(true)));
+                WbContent.Invoke(new Action(() => WbContent.DocumentText = news["contents"].AsString()));
+                //LblContent.Text = @Html.Raw(news["contents"].AsString());
+            }
+        }
+    }
+}
